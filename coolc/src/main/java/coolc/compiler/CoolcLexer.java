3a9a66d2c65ab4d46d217452d13e81d04c5ca607
luis&cristian <a01337755@gmail.com>
2017-02-10 01:30:33 -0600

    implementing states in comments

--- coolc/src/main/java/coolc/compiler/CoolcLexer.java
+++ coolc/src/main/java/coolc/compiler/CoolcLexer.java
@@ -7,14 +7,24 @@ import java.lang.reflect.Method;
 
 import coolc.compiler.autogen.lexer.Lexer;
 import coolc.compiler.autogen.lexer.LexerException;
+import coolc.compiler.autogen.node.EOF;
+import coolc.compiler.autogen.node.TCommentEnd;
+import coolc.compiler.autogen.node.TCommentIn;
+import coolc.compiler.autogen.node.TCommentStart;
+import coolc.compiler.autogen.node.TErrorStarLpar;
+import coolc.compiler.autogen.node.TLcommentStart;
+import coolc.compiler.autogen.node.TLcommentEnd;
 import coolc.compiler.autogen.node.Token;
 import coolc.compiler.autogen.parser.Parser;
+import coolc.compiler.util.Messages;
+import coolc.compiler.util.*;
 
 public class CoolcLexer extends Lexer {
 	private Parser parser;
 	private Method index;
 	private Token lastToken;
 	private PrintStream out;
+	int comments = 0;
 
 	public CoolcLexer(PushbackReader in, PrintStream out) {
 		super(in);
@@ -41,12 +51,42 @@ public class CoolcLexer extends Lexer {
 	public Token next() throws LexerException, IOException {
 		return super.next();
 	}
+	
+	protected void lexx(){
+		//comments
+		if (token instanceof TCommentStart) {
+			comments++;
+		}
+
+		else if (token instanceof TCommentIn) {
+			comments++;
+		} 
+
+		else if (token instanceof TCommentEnd) {
+			comments--;
+			if (comments == 0) {
+				state = State.INITIAL;
+			} else {
+				state = State.COMMENT;
+			}
+		}
+		//comments errors
+		commenterrors();//
+	}
+	protected void commenterrors(){
+		 if (token instanceof TErrorStarLpar) {
+			out.format(Messages.getString("Coolc.lexer.unmatchedCloseComment"), token.getLine()); 
+
+		 }
+	}
 
 	@Override
 	protected void filter() throws LexerException, IOException {
-		while( ignore(token)){
-				token = super.getToken();}
-				
+			lexx();
+			while (ignore(token)) {
+			token = getToken();
+			lexx();
+			}
 	}
 
 	public void setParser(Parser p) {
